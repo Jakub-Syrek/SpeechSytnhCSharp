e33aca0fb3710d9032dcbe520aafbd27bd08a3c7
Jakub-Syrek <40210613+Jakub-Syrek@users.noreply.github.com>
2019-01-27 19:00:57 +0100

    1 Comm : Form in XAML + class audio

---
+++ WpfDemo/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+        public string prefferedVoice = "Microsoft David Desktop";
+        private void ReadContent_Click(object sender, RoutedEventArgs e)
+        {           
+                using (TasksVoiceSynthesis taskObj = new TasksVoiceSynthesis($"{ Text.Text}", prefferedVoice))
+                {
+                    //taskObj.text = $"{ Text.Text}";
+                    taskObj.PlayTasks(prefferedVoice);
+                }            
+        }        
+        private void ChangeVoice_Click(object sender, RoutedEventArgs e)
+        {
+            using (TasksVoiceSynthesis taskObj = new TasksVoiceSynthesis($"{ Text.Text}", prefferedVoice))
+            {
+                //taskObj.text = $"{ Text.Text}";
+                //taskObj.PlayTasks();
+                //taskObj.ReturnInstalledVoices();
+                List<string> lista = taskObj.Voices;              
+                
+                string currentVoice = taskObj.CurrentVoice();
+                for (int i = 0;i <= lista.Count-1;i++)
+                {
+                    if (prefferedVoice != lista[i])
+                    {
+                        prefferedVoice = lista[i];
+                        break;
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WpfDemo/WpfDemo/TasksVoiceSynthesis.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WpfDemo
+{
+    public class TasksVoiceSynthesis : IDisposable
+    {
+        public string Text { get; set; } = "";
+          
+        public string PrefferedVoice { get; set; } = "Microsoft David Desktop";
+
+
+
+
+
+        // Initialize a new instance of the Speech Synthesizer.
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+
+
+
+        public TasksVoiceSynthesis(string text, string PrefferedVoice)
+        {
+            
+            {
+                this.Text = text;
+                this.PrefferedVoice = PrefferedVoice;
+            }
+        }
+        
+
+        public string CurrentVoice()
+        {
+            
+                    
+                    string CurrentVoice = synth.Voice.Name;
+                    return CurrentVoice;
+                    
+                
+        }
+        public List<string> Voices
+        {
+            get
+            {
+                //Initialize string array
+                List<string> voicesList = new List<string>();
+                //Querry all installed voices
+                foreach (InstalledVoice voice in synth.GetInstalledVoices())
+                {
+                    VoiceInfo info = voice.VoiceInfo;
+                    voicesList.Add(info.Name);
+                }
+                //return string list with all voices
+                return voicesList;
+            }
+        }        
+        public  void PlayTasks()
+        {
+
+            
+
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+            // Create a prompt from a string.  
+            Prompt voice = new Prompt(Text);
+            // Speak a string.  
+            synth.SpeakAsync(voice);            
+        }
+        public void PlayTasks(string PrefferedVoice)
+        {
+        
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+            // Create a prompt from a string.  
+            Prompt voice = new Prompt(Text);
+            //change Voice
+            synth.SelectVoice(PrefferedVoice);
+            // Speak a string.  
+            synth.SpeakAsync(voice);
+            //synth.Speak(voice);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~TasksVoiceSynthesis() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
+/* 
+   private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {            
+            TasksVoiceSynthesis taskObj = new TasksVoiceSynthesis($"{ Text.Text}") ;
+            //taskObj.text = $"{ Text.Text}";
+            taskObj.PlayTasks();
+        }
+
+    public void List<string> ReturnInstalledVoices()
+        {
+            // Initialize a new instance of the Speech Synthesizer.
+            SpeechSynthesizer synth1 = new SpeechSynthesizer();
+            //Initialize string array
+            List<string> voicesList = new List<string>();
+            foreach (InstalledVoice voice in synth1.GetInstalledVoices())            
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                voicesList.Add(info.Name);
+            }
+            return voicesList;
+        }
+
+     public static List<string> ReturnInstalledVoices()
+        {
+            // Initialize a new instance of the Speech Synthesizer.
+            SpeechSynthesizer synth1 = new SpeechSynthesizer();
+            //Initialize string array
+            List<string> voicesList = new List<string>();
+            foreach (InstalledVoice voice in synth1.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                voicesList.Add(info.Name);
+            }
+            return voicesList;
+        }
+        */
+
+
